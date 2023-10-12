8d9a6f43047d6b31d72f2c734db30a1a31565494
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-13 08:40:29 +0900

    Add 200px height and width to class container

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 function hello(){
     console.log('Hello World!');
 }
+
+const container = document.querySelector('.container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is a div from DOM methods';
+container.appendChild(content);
