6860834ea13ef1ea956532db0fab6119d2cb8a30
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-13 14:04:33 +0900

    Create function addDiv that have class content

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ function hello(){
     console.log('Hello World!');
 }
 
+function addDiv(){
+    content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = 'This is a div from DOM methods';
+    container.appendChild(content);
+}
+
 const container = document.querySelector('.container');
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is a div from DOM methods';
-container.appendChild(content);
+let content;
+
+
