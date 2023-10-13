5c11e882bdc6ef367551043844296ddddf0abe73
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-13 14:29:01 +0900

    Add 50px width and height to class content and change its textContent to 'Child Box'

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function addDiv(){
     content = document.createElement('div');
     content.classList.add('content');
-    content.textContent = 'This is a div from DOM methods';
+    content.textContent = 'Child Box';
     container.appendChild(content);
 }
 
@@ -10,7 +10,7 @@ let content;
 
 let gridCount = 4;
 
-for (i=0;i <= gridCount**2;i++){
+for (i=0 ; i < gridCount**2 ; i++){
     addDiv()
     console.log(i);
 }
