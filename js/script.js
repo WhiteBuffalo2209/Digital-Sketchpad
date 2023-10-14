1c676bbf28b676efcd8f19158b88432fbe42954e
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-14 13:29:56 +0900

    Add width and height of content box in function addDiv

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ function addDiv(){
     content = document.createElement('div');
     content.classList.add('content');
     content.textContent = '';
+    content.style.width = `${(600/gridCount)-2}px`;  //content border 2px
+    content.style.height = `${(600/gridCount)-2}px`;
     container.appendChild(content);
 }
 
 const container = document.querySelector('.container');
 let content;
 
-let gridCount = 16;
+let gridCount = 8;
 
 for (i=0 ; i < gridCount**2 ; i++){
     addDiv()
