cf3fd6efe500b064f6b86d513697aca10fa0a368
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-14 13:09:01 +0900

    Deduct 2px in width and height of class content for border offset

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 function addDiv(){
     content = document.createElement('div');
     content.classList.add('content');
-    content.textContent = 'Child Box';
+    content.textContent = '';
     container.appendChild(content);
 }
 
 const container = document.querySelector('.container');
 let content;
 
-let gridCount = 4;
+let gridCount = 16;
 
 for (i=0 ; i < gridCount**2 ; i++){
     addDiv()
-    console.log(i);
+    console.log(`${i+1} boxes`);
 }
 
 
