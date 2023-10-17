2adbab2caf3ad149b62a7ae2a9d3473427f78df6
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-10-17 13:52:55 +0900

    Remove class up, replace it with random color using style.background, and create function generateRandomColor

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ function addDiv(){
     content.style.width = `${(600/gridCount)-2}px`;  //content border 2px
     content.style.height = `${(600/gridCount)-2}px`;
     container.appendChild(content);
-
 }
 
 const container = document.querySelector('.container');
@@ -20,17 +19,23 @@ for (i=0 ; i < gridCount**2 ; i++){
 
 const triggers = document.querySelectorAll('.content');
 triggers.forEach(trigger => trigger.addEventListener('mousedown', () => {
-    trigger.classList.add('up');
+    trigger.style.backgroundColor = generateRandomColor();
 }));
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', (e) => {
-    console.log(e);
     if (e.buttons === 1) {
-        trigger.classList.add('up');
+        trigger.style.backgroundColor = generateRandomColor();
     }
-    //e.buttons === 1
-    //target === div.content.up
 }));
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF;
+    let randomNumber = Math.random()*maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6,0);
+    return `#${randColor.toUpperCase()}`;
+}
 
 
 
